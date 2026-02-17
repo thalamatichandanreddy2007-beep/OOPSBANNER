a903aa265adcb4f166276f6ea1eb9c67d38e3ba8
thalamatichandanreddy2007-beep <thalamatichandanreddy.2007@gmail.com>
2026-02-17 09:33:14 +0530

    HELLO OOPS

---
+++ OOPSBANNER.java
@@ -0,0 +1,23 @@
+public class OOPSBANNER {
+    
+    //STEPS TO PUSH INTO GITHUB 
+
+    // U untracked 
+
+    // write -------- git add . 
+    
+    // A ADDED
+
+
+    // M MODIFIED 
+
+    // git commit -m 
+
+    //now we should branch 
+    //git checkout -b feature/UC1-PrintOOPS (this uc1 is from the given question )
+    public static void main(String[] args){
+        System.out.println("OOPS");
+        
+    }
+    
+}
